23ac7fb6b4a6409fca42d17325b4e1473b4d3420
meryeliz <lizzybtk@outlook.com>
2023-04-02 18:19:18 -0300

    se trabajo en js funciones

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,28 @@ console.log("Inicio de pruebas");
 let nombre = "";
 let apellido = "";
 let edad = 0;
-let cantidad = 2;
+let cantidad = 1;
+let dia = "";
 
-alert("¡¡¡Bienvenido!!!");
-
+alert("¡¡¡Inscripciones!!!");
 
 for (let usuario = 1; usuario <= cantidad; usuario++){
+    // let continuar = prompt("Desea continuar: SI / NO");
+    // if (continuar.toUpperCase == "NO") {
+    //     break;
+    // }
     alert("Vamos a inciar la inscripción de tu curso...");
     pedirDatos();
 
-    // Se controla la edad 
-    if(isNaN(edad)) {
-        cantidad += 1;
-        alert("No es un numero");
-    }
     if(edad < 18) {
         cantidad += 1;
         alert("No se puede asignar un turno. Debes ser mayor de edad.");
     }
     if (edad >= 18 ) {
         alert("Ahora vamos a asignarte un turno");
+        // Asignamos dia y horarios
+        asignarTurno();
+        
     }
 
     // cantidad de turnos restantes
@@ -38,46 +40,84 @@ for (let usuario = 1; usuario <= cantidad; usuario++){
 }
 
 function pedirDatos() {
-    // Pedimos el nomnbre y apellido del usuario 
-    nombre = prompt("¿Podrías decirme tu nombre?");
-    apellido = prompt("¿Cúal es tu apellido ".concat(nombre, "?"));
-    // Se ingresa la edad del usuario
-    edad = parseInt(prompt("Ingrese su edad"));
+    // Pedimos el nomnbre, apellido y edad al usuario 
+    cargarNombre();
+    cargarApellido();
+    cargarEdad();
 
 }
 
-function asignarTurno() {
-    alert("Los dias disponibles son LUNES, MARTES Y MIERCOLES");
-    let dia = prompt("Ingrese el día que desea iniciar el curso");
-
-    while (dia.toUpperCase() != "MARTES" || dia.toUpperCase() != "JUEVES" || dia.toUpperCase() != "DOMINGO") {
-        switch (dia.toUpperCase()) {
-        case "LUNES":
-            alert(nombre + ", te espero el Lunes a las 18hs." );
-            break;
-        case "MIERCOLES":
-                alert(nombre + ", te espero el Miércoles a las 18hs." );
-            break;
-        case "VIERNES":
-                alert(nombre + ", te espero el Viernes a las 18hs." );
-            break;
-        case "SABADO":
-                alert(nombre + ", te espero el Sábado a las 18hs." );
-            break;
+// Controlar nombre
+function cargarNombre() {
+    let continuar = true;
+    while (continuar) {
+        let testNombre = prompt("¿Podrías decirme tu nombre?");    
+        if (testNombre.length != 0) {
+            nombre = testNombre;
+            continuar = false;
+        } else {
+            alert("Debe ingresar su nombre...")
         }
     }
-    
 }
 
+// Controlar apellido
+function cargarApellido() {
+    let continuar = true;
+    while (continuar) {
+        let testApellido = prompt("¿Cúal es tu apellido ".concat(nombre, "?")); 
+        if (testApellido.length != 0) {
+            apellido = testApellido;
+            continuar = false;
+        } else {
+            alert("Debe ingresar su apellido...")
+        }
+    }
+}
 
-// let asignarDia = pedirDatos();
-// alert(asignarDia);
+// Controlar edad
+function cargarEdad() {
+    let continuar = true;
+    while (continuar) {
+        testEdad = parseInt(prompt("Ingrese su edad"));
+        if(isNaN(testEdad) && testEdad.length != 0) {
+            alert("Debe ingresar su edad (solo se permiten números)");
+        } else {
+            edad = testEdad;
+            continuar = false;
+        }
+    }
+}
 
-// function controlarEdad(suEdad) {
-//     if (isNaN(suEdad)) {
-//         return "Error. Debe ingresar un número para su edad."
-//     }
-// }
+//Funcion para otorgar turno
+function asignarTurno() {
+    do {
+        alert("Los dias disponibles son LUNES, MIERCOLES, VIERNES Y SABADO");
+        dia = prompt("Ingrese el día que desea iniciar el curso");
+        switch (dia.toUpperCase()) {
+            case "LUNES":
+                alert(nombre + ", te espero el Lunes a las 18hs." );
+                break;
+            case "MIERCOLES":
+                    alert(nombre + ", te espero el Miércoles a las 18hs." );
+                break;
+            case "VIERNES":
+                    alert(nombre + ", te espero el Viernes a las 18hs." );
+                break;
+            case "SABADO":
+                    alert(nombre + ", te espero el Sábado a las 18hs." );
+                break;
+            default:
+                let incorrecto = true;
+                if (incorrecto) {
+                    cantidad += 1;
+                    alert("EL día ingresado es incorrecto");
+                }
+                break;
+        }
+    } while (dia.toUpperCase() == "MARTES" || dia.toUpperCase() == "JUEVES" || dia.toUpperCase() == "DOMINGO");
+    
+}
 
 // pruebas por consola
 console.log("El nombre ingresado es: ".concat(nombre," ",apellido));
